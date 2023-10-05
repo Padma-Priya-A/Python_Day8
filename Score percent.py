0977cc91f4571fc592e4001f5cd59da92751081c
Padma_Priya <116820600+Padma-Priya-A@users.noreply.github.com>
2023-10-05 22:11:07 +0530

    Update Score percent.py

--- Score percent.py
+++ Score percent.py
@@ -1,3 +1,7 @@
+# Get input for salary and age.
+# If salary greater than or equal to 20000 or age less than or equal to 25, get input for required loan amount. If not print you are not eligible for loan. 
+# If required loan amount is less than or equal to 50,000 print You are eligible for loan. If it is greater than 50,000 print maximum loan amount is 50000.
+
 score=int(input("Enter the Score Percent:"))
 if(score>=70):
     name=input("enter the name:")
