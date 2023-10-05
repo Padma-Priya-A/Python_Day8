ec5d078b73fce899492b17d0728a486ba9b33dd6
Padma_Priya <116820600+Padma-Priya-A@users.noreply.github.com>
2023-10-05 22:08:12 +0530

    Update Mini calculator.py

--- Mini calculator.py
+++ Mini calculator.py
@@ -1,3 +1,5 @@
+#Make a mini calculator. Get input for 2 numbers a and b. Get input from user whether to add/sub/mul/div. If user selects add then add the two number and Print the result.
+
 a=int(input("Enter the value of a:"))
 b=int(input("Enter the value of b:"))
 op=input("add/sub/mul/div:")
